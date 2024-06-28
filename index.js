f5f3f7e4570cd30241004bbfb5c5be42060c6e0a
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-28 08:24:43 +0300

    [] Used when passing an arguement

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+let employee = {
+  name: "Johnson",
+  streetAddress: 2355,
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let newemp = { ...employee };
+  newemp[key] = value;
+  return newemp;
+}
+console.log(updateEmployeeWithKeyAndValue(employee, "name", "Omwoyo"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+console.log(
+  destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "mancella")
+);
+
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmp = { ...employee };
+  delete newEmp[key];
+  return newEmp;
+}
+console.log(deleteFromEmployeeByKey(employee, "name"));
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
